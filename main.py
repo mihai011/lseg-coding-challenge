f7f48c44c514f06eacfa5a1310542ca5fa47258f
mika <mika@pop-os.localdomain>
2022-08-17 17:29:20 +0300

    Added code and tests

---
+++ main.py
@@ -0,0 +1,114 @@
+"""
+Resolving LSEG coding challenge.
+
+"""
+
+import numpy as np
+from tqdm import tqdm
+
+from utils import increase, init, zeros, ones, toogle, increase, decrease
+
+
+TURN_ON = "turn on"
+TURN_OFF = "turn off"
+TOGGLE = "toggle"
+
+
+def read_input(file):
+    """
+    Opens, read file and parse the contents.
+    Args:
+        file (str): path to the file
+    Returns:
+        list: list of parsed instructions
+    """
+
+    instructions = []
+
+    action_instructions = {}
+    action_instructions[TURN_ON] = TURN_ON
+    action_instructions[TURN_OFF] = TURN_OFF
+    action_instructions[TOGGLE] = TOGGLE
+
+    with open(file, encoding="utf-8") as input_file:
+        for line in input_file.readlines():
+
+            instruction = {}
+            words = line.split(" ")
+            pos_2 = eval(words[-1])
+            pos_1 = eval(words[-3])
+
+            instruction["pos1"] = pos_1
+            instruction["pos2"] = pos_2
+
+            for pos_instruction in [TURN_ON, TURN_OFF, TOGGLE]:
+                if line.startswith(pos_instruction):
+                    instruction["action"] = action_instructions[pos_instruction]
+
+            instructions.append(instruction)
+
+    return instructions
+
+
+def resolve(file_name, part_2=False):
+    """
+    Args:
+        file_name
+        part_2 (bool, optional): Bool values for switching on resolving part 2. Defaults to False.
+    Returns:
+        int: sum of all elements in the grid
+    """
+
+    instructions = read_input(file_name)
+    grid = init((1000, 1000))
+
+    action_functions = {TURN_ON: ones, TURN_OFF: zeros, TOGGLE: toogle}
+
+    if part_2:
+        action_functions[TURN_ON] = lambda area: increase(area, 1)
+        action_functions[TURN_OFF] = lambda area: decrease(area, 1)
+        action_functions[TOGGLE] = lambda area: increase(area, 2)
+
+    for ins in tqdm(instructions):
+        ins["pos2"] = ins["pos2"][0] + 1, ins["pos2"][1] + 1
+        pos10, pos20, pos11, pos21 = (
+            ins["pos1"][0],
+            ins["pos2"][0],
+            ins["pos1"][1],
+            ins["pos2"][1],
+        )
+        work_area = grid[pos10:pos20, pos11:pos21]
+        grid[pos10:pos20, pos11:pos21] = action_functions[ins["action"]](work_area)
+
+    return np.sum(grid)
+
+
+def test_read():
+    """_summary_"""
+    instructions = read_input("test_input.txt")
+    expected_instruction = [
+        {"action": TURN_ON, "pos1": (0, 0), "pos2": (999, 999)},
+        {"action": TURN_OFF, "pos1": (499, 499), "pos2": (500, 500)},
+        {"action": TOGGLE, "pos1": (0, 499), "pos2": (999, 500)},
+    ]
+
+    assert instructions == expected_instruction
+
+
+def test_integration():
+    """_summary_"""
+
+    res_1 = resolve("test_input.txt")
+    assert res_1 == 998004
+    res_2 = resolve("test_input.txt", True)
+    assert res_2 == 1003996
+
+
+if __name__ == "__main__":
+
+    res_1 = resolve("test_input.txt", False)
+    print(res_1)
+    res_2 = resolve("test_input.txt", True)
+    print(res_2)
+    real_res = resolve("real_input.txt", True)
+    print(real_res)
---
+++ utils.py
@@ -0,0 +1,129 @@
+"""_
+Utils module function
+"""
+
+import numpy as np
+
+
+
+
+def init(shape):
+    """
+    Initialize a numpy array of zeroes with the given shape.
+
+    Args:
+        shape (tuple): dimensions for the
+
+    Returns:
+        np.array: numpy array of zeroes
+    """
+
+    return np.zeros(shape, int)
+
+
+def ones(area):
+    """
+    Initialize a numpy array of ones with the same shape as area parameter.
+
+    Args:
+        area (np.array): area of work
+
+    Returns:
+        np.array: numpy array of ones
+    """
+
+    return np.ones(area.shape)
+
+
+def zeros(area):
+    """
+    Initialize a numpy array of zeros with the same shape as area parameter.
+    Args:
+        area (np.array): area of work
+
+    Returns:
+        np.array: numpy array of zeros
+    """
+
+    return np.zeros(area.shape)
+
+
+def toogle(area):
+    """
+    Toggles values in an numpy array between 0 and 1.
+
+    Args:
+        area (np.array): area of work
+
+    Returns:
+        np.array: toggled numpy array
+    """
+
+    toogle_func = np.vectorize(lambda x: 1 - x)
+    return np.apply_along_axis(toogle_func, 1, area)
+
+
+def increase(area, amount):
+    """
+    Increases values in an numpy array by amount.
+
+    Args:
+        area (np.array): area of work
+        amount (int): amount by which we increase
+
+    Returns:
+        np.array: increased numpy array
+    """
+
+    inc_func = np.vectorize(lambda x: x + amount)
+    return np.apply_along_axis(inc_func, 1, area)
+
+
+def decrease(area, amount):
+    """
+    Decreases values in an numpy array by amount.
+    Args:
+        area (np.array): area of work
+        amount (int): amount by which we decrease
+    Returns:
+        np.array: decreased numpy array
+    """
+
+    dec_func = np.vectorize(lambda x: x - amount)
+    return np.apply_along_axis(dec_func, 1, area)
+
+
+def decrease_by_one(area):
+    """
+    Decreases values in an numpy array by 1.
+    Args:
+        area (np.array): area of work
+    Returns:
+        np.array: decreased numpy array by one
+    """
+
+    return decrease(area, 1)
+
+
+def increase_by_one(area):
+    """
+    Increases values in an numpy array by 1.
+    Args:
+        area (np.array): area of work
+    Returns:
+        np.array:
+    """
+
+    return increase(area, 1)
+
+
+def increase_by_two(area):
+    """
+    Increases values in an numpy array by 2.
+    Args:
+        area (np.array): area of work
+    Returns:
+        _type_: _description_
+    """
+
+    return increase(area, 2)
